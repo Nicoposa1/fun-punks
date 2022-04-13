2c2e1e734950c6128002e126253dac0f0451dd59
Nicolas Posa <nicolasposa@Nicolass-Air.home>
2022-04-13 00:04:23 -0300

    add metadata and Base64 standard

--- contracts/FunPunks.sol
+++ contracts/FunPunks.sol
@@ -5,31 +5,61 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
-
+import "./Base64.sol";
 
 contract FunPunks is ERC721, ERC721Enumerable, PaymentSplitter {
-  using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
 
-  Counters.Counter private _idCounter;
-  uint256 public maxSuply;
+    Counters.Counter private _idCounter;
+    uint256 public maxSuply;
 
-  constructor(address[] memory _payees, uint256[] memory _shares, uint256 _maxSuply) ERC721("FunPunks", "FNPKS") PaymentSplitter(_payees, _shares) payable{
-    maxSuply = _maxSuply;
-  }
+    constructor(
+        address[] memory _payees,
+        uint256[] memory _shares,
+        uint256 _maxSuply
+    ) payable ERC721("FunPunks", "FNPKS") PaymentSplitter(_payees, _shares) {
+        maxSuply = _maxSuply;
+    }
 
-  function mint() public payable {
-    require(msg.value >= 50000000000000000, "you neet 0.05 ETH to mint the PinaPunks");
-    _idCounter.increment();
-    uint256 current = _idCounter.current(); 
-    require(current < maxSuply, "No FunPunks left to mint");
-    _safeMint(msg.sender, current);
-  }
+    function mint() public payable {
+        require(
+            msg.value >= 50000000000000000,
+            "you neet 0.05 ETH to mint the PinaPunks"
+        );
+        _idCounter.increment();
+        uint256 current = _idCounter.current();
+        require(current < maxSuply, "No FunPunks left to mint");
+        _safeMint(msg.sender, current);
+    }
 
-  //Override require
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
     {
+        require(
+            _exists(tokenId),
+            "ERC721 Metadata; URI query for nonexistent token"
+        );
+        string memory jsonURI = Base64.encode(
+          abi.encodePacked(
+            '{ "name": "FunPunks #',
+                tokenId,
+                '", "description": "An incredible NFT collection", "image": "',
+                "// TODO: Calculate image URL",
+                '"}'
+            )
+          );
+          return string(abi.encodePacked("data:application/json;base64", jsonURI));
+    }
+
+    //Override require
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
@@ -41,5 +71,4 @@ contract FunPunks is ERC721, ERC721Enumerable, PaymentSplitter {
     {
         return super.supportsInterface(interfaceId);
     }
-
 }
