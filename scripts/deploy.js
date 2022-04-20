1bb83617e5feedd8ab998d614d9ba780126a9fb6
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-04-19 23:39:31 -0300

    smart contract deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const deploy = async () => {
   console.log("Deploying contract with the account", deployer.address);
 
   const FunPunks = await ethers.getContractFactory("FunPunks");
-  const deployed = await FunPunks.deploy();
+  const deployed = await FunPunks.deploy(10000);
 
   console.log('Fun pubks is deployed at', deployed.address);
 }
